75fdc3af65da9502a7a58eac4399f102c674a557
Dhruv Jain <dhruvjainpenny@gmail.com>
2022-04-05 21:56:33 +0530

    clean

--- src/helpers/telemetry.ts
+++
@@ -1,7 +0,0 @@
-export const captureEvent = (event: string, payload?: any) => {
-  import("posthog-js")
-    .then(module => module.default)
-    .then(posthog => {
-      posthog.capture(event, payload);
-    });
-};
